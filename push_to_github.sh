285ea5ca497969b14a7fa50f2ab5b76d40e93c3b
at1a5 <anrist@vip.qq.com>
2020-02-03 00:50:43 +0800

    ENH: add script for git update
    Signed-off-by: at1a5 <anrist@vip.qq.com>

---
+++ push_to_github.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: push_to_github.sh
+# Author: Xingcheng Liu
+# Email:  anrist@vip.qq.com
+# Created Time: Sun 02 Feb 2020 04:20:47 PM CST
+#########################################################################
+#!/bin/bash
+git push origin master
