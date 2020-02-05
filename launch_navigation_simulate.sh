e30b0fa590274a4ac02d010d7f4edfbc55c350aa
at1a5-lxc <anrist@vip.qq.com>
2020-02-05 14:26:21 +0800

    ENH: distinguish simulate script for local/remote simulation
    Signed-off-by: at1a5-lxc <anrist@vip.qq.com>

--- launch_navigation_simulate.sh
+++ launch_navigation_simulate.sh
@@ -1,1 +1,5 @@
-roslaunch robot_navigation robot_navigation.launch simulation:=true open_rviz:=true
+if [ $# -eq 0 ]; then
+    roslaunch robot_navigation robot_navigation.launch simulation:=true open_rviz:=true
+else
+    roslaunch robot_navigation robot_navigation.launch simulation:=true open_rviz:=false
+fi
